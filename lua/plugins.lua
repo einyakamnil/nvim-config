a575040d5a643e45df53878fb8852dc2a55fd5d9
EinYakAmNil <einyak@gmail.com>
2022-10-07 18:04:43 +0200

    add lualine as statusline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,10 @@
 return require('packer').startup(function(use) 
 --Packer can manage itself
     use 'wbthomason/packer.nvim'
+
+    use {
+	'nvim-lualine/lualine.nvim',
+	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)
+
